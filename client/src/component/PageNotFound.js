0cef780bccc59e4dec58e297b1804621a691b9a1
miten09 <mitensoni108@gmail.com>
2023-07-14 19:28:19 +0530

    404 page & forgot_reset password completed

--- client/src/component/PageNotFound.js
+++ client/src/component/PageNotFound.js
@@ -1,10 +1,26 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 function PageNotFound() {
   return (
-    <>
-      <h1>Page Not Found</h1>
-    </>
+    <div
+      style={{
+        marginTop: "5%",
+        marginLeft: "25%",
+      }}
+    >
+      <Typography
+        variant="h3"
+        sx={{
+          backgroundColor: "white",
+          padding: "10%",
+
+          width: "500px",
+        }}
+      >
+        404 Page Not Found
+      </Typography>
+    </div>
   );
 }
 
